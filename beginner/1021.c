12dd666a900bc6d22bd77b71e3d7d6b404e5bcbc
Tomcat-42 <PabloASHugen@protonmail.com>
2019-03-28 22:03:16 -0300

    Novos exercícios

--- beginner/1021.c
+++ beginner/1021.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-	float val,moedas;
-	int val_s_moedas;
-	
-	scanf("%f",&val);
+	double money;
+	int money_int;
 
-	if(val<0.0 || val>1000000.0)
+	scanf("%lf",&money);
+	
+	if(money<0.0 || money>1000000.0)
 	{
 		return 1;
 	}
-	
-	val_s_moedas = val;
-	moedas = (val - val_s_moedas);
+
+	money_int = (money * 1000.0);
 	
 	printf("NOTAS:\n");
 
-	printf("%d nota(s) de R$ 100.00\n",(val_s_moedas/100));
-	val_s_moedas %= 100;
+	printf("%d nota(s) de R$ 100.00\n",(money_int/100000));
+	money_int %= 100000;
 
-	printf("%d nota(s) de R$ 50.00\n",(val_s_moedas/50));
-	val_s_moedas %= 50;
+	printf("%d nota(s) de R$ 50.00\n",(money_int/50000));
+	money_int %= 50000;
 	
-	printf("%d nota(s) de R$ 20.00\n",(val_s_moedas/20));
-	val_s_moedas %= 20;
+	printf("%d nota(s) de R$ 20.00\n",(money_int/20000));
+	money_int %= 20000;
 
-	printf("%d nota(s) de R$ 10.00\n",(val_s_moedas/10));
-	val_s_moedas %= 10;
+	printf("%d nota(s) de R$ 10.00\n",(money_int/10000));
+	money_int %= 10000;
 
-	printf("%d nota(s) de R$ 5.00\n",(val_s_moedas/5));
-	val_s_moedas %= 5;
+	printf("%d nota(s) de R$ 5.00\n",(money_int/5000));
+	money_int %= 5000;
 
-	printf("%d nota(s) de R$ 2.00\n",(val_s_moedas/2));
-	val_s_moedas %= 2;
+	printf("%d nota(s) de R$ 2.00\n",(money_int/2000));
+	money_int %= 2000;
 
-	
 	printf("MOEDAS:\n");
-	
-	
-	printf("%d moeda(s) de R$ 1.00\n",(val_s_moedas/1));
+		
+	printf("%d moeda(s) de R$ 1.00\n",(money_int/1000));
+	money_int %= 1000;
 
-	printf("%d moeda(s) de R$ 0.50\n",fmod(moedas, 0.50));
-	moedas = fmod(moedas,0.5);
+	printf("%d moeda(s) de R$ 0.50\n",(money_int/500));
+	money_int %= 500;
+
+	printf("%d moeda(s) de R$ 0.25\n",(money_int/250));
+	money_int %= 250;
+
+	printf("%d moeda(s) de R$ 0.10\n",(money_int/100));
+	money_int %= 100;
+
+	printf("%d moeda(s) de R$ 0.05\n",(money_int/50));
+	money_int %= 50;
+
+	printf("%d moeda(s) de R$ 0.01\n",(money_int/10));
 }
