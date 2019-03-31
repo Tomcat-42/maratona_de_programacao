52e7f4a18cb219e5af7c14becbd2ad8fab1d3375
Tomcat-42 <PabloASHugen@protonmail.com>
2019-03-31 17:05:46 -0300

    Novos exercícios.

--- beginner/1984.c
+++ beginner/1984.c
@@ -2,20 +2,16 @@
 
 int main()
 {
-	int resto=0,inv=0;
+	
 	unsigned long long num;
 	
 	scanf("%llu",&num);
 
 	while(num)
 	{
-		resto = num%10;
-		inv = inv * 10 + resto;
+		printf("%llu",(num%10));
 		num/=10;
 			
 	}
-	printf("%d\n",inv);
-
-
-
+	printf("\n");
 }
