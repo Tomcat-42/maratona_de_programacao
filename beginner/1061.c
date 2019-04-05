f5708ef6503f42948da4655a191f57d4338b5db7
Tomcat-42 <pablohuggem@gmail.com>
2019-04-05 17:12:24 -0300

    Novos exercícios.

--- beginner/1061.c
+++ beginner/1061.c
@@ -15,20 +15,27 @@ int main()
 	sscanf(fgets(input,MAX_INPUT_NUM,stdin),"Dia %d ",&fim[0]);
 	//le o horario do fim
 	sscanf(fgets(input,MAX_INPUT_NUM,stdin),"%d : %d : %d ",&fim[1],&fim[2],&fim[3]);
-	
-	//segundos
-	(inicio[3] <= fim[3]) ? (tempo_total[3] = fim[3] - inicio[3]) : (tempo_total[3] = (60 - inicio[3] + fim[3]));
-	
-	//minutos
-	(inicio[2] <= fim[2]) ? (tempo_total[2] = fim[2] - inicio[2]) : (tempo_total[2] = (60 - inicio[2] +  fim[2]));
-	
-	//horas
-	(inicio[1] <= fim[1]) ? (tempo_total[1] = fim[1] - inicio[1]) : (tempo_total[1] = (24 - inicio[1] + fim[1]));
 
-	//dias
-	(inicio[1] <= fim[1]) ? (tempo_total[0] = fim[0] - inicio[0]) : (tempo_total[0] = fim[0] - inicio[0] - 1);
+
+	//dia
+	tempo_total[0] = fim[0] - inicio[0];
+
+	//horas
+	if(inicio[1] < fim[1])
+	{
+		tempo_total[1] = fim[1] - inicio[1];
+
+	}	
+	else
+	{
+		tempo_total[1] = (24 - inicio[1]) + fim[1];
+		tempo_total[0]--;
+	}
+
 
 	printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n",tempo_total[0],tempo_total[1],tempo_total[2],tempo_total[3]);
 	
+
+		
 }
 
